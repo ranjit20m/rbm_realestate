7b9789382902eaf1d105c9e1fdb02a8be22eb2e0
Ranjit Madame <ranjitkumar.madame@mphasis.com>
2022-04-17 22:26:51 +0530

    first commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) =>  {
     const { data } = await axios.get((url), {
         headers: {
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-            'X-RapidAPI-Key': '71cdb9b299msh70e73490afb58dfp12e2f9jsn980e439f9e25'
+            'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY
           }
     })
 
